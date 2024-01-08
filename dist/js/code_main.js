019d97e53a89f0831a16afc1aa741042cba2e9a3
Carlos Aleman <caleman9791@gmail.com>
2024-01-08 06:25:05 -0400

    Url localhot

--- dist/js/code_main.js
+++ dist/js/code_main.js
@@ -1,10 +1,10 @@
-if (!(window.location.href == "http://localhost/github/caleman9791/notas/htdocs/index.html")) {
+if (!(window.location.href == "http://localhost/github/pruebas/index.html")) {
 
 
 	// window.location.href = "http://localhost/app/Notas/htdocs/index.html";
-	window.location.href = "http://localhost/github/caleman9791/notas/htdocs/index.html";
+	window.location.href = "http://localhost/github/pruebas/index.html";
 
 	// /opt/lampp/htdocs/github/caleman9791/Notas/htdocs/index.html
 }
 
-// /opt/lampp/htdocs/github/caleman9791/notas/htdocs/index.html
+// /opt/lampp/htdocs/github/pruebas/index.html
