b92c020f410806880001e65824663273bd477f32
Carlos Aleman <caleman9791@gmail.com>
2024-01-08 06:23:12 -0400

    Plantilla

---
+++ dist/js/code_main.js
@@ -0,0 +1,10 @@
+if (!(window.location.href == "http://localhost/github/caleman9791/notas/htdocs/index.html")) {
+
+
+	// window.location.href = "http://localhost/app/Notas/htdocs/index.html";
+	window.location.href = "http://localhost/github/caleman9791/notas/htdocs/index.html";
+
+	// /opt/lampp/htdocs/github/caleman9791/Notas/htdocs/index.html
+}
+
+// /opt/lampp/htdocs/github/caleman9791/notas/htdocs/index.html
